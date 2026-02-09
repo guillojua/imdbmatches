e45bc9c123c0602f5021af8e2c7b2b6130acd037
Guillermo <113069296+guillojua@users.noreply.github.com>
2026-02-08 21:31:35 -0300

    Update main.jsx

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./App.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { Analytics } from '@vercel/analytics/react'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
+    <Analytics />
   </React.StrictMode>
-);
+)
